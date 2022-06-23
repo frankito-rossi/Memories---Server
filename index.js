5be5b8af47ad5f687bfd2487c303a3a66238472a
Franco Rossi <francorossi1996@gmail.com>
2022-06-23 11:26:04 -0300

    server

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello to Memories API');
+});
+
+//const CONNECTION_URL = 'mongodb+srv://francorossi:francorossi123@cluster0.9exmz.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { /*useNewUrlParser: true, useUnifiedTopology: true*/})
+  .then(() => app.listen(PORT, () => console.log( `Server running on port: ${PORT}`)))
+  .catch((error) => console.log("Error: ",error.message));
+
+//mongoose.set('useFindAndModify', false);
